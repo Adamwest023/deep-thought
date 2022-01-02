969282e5d6b535131a1dc2248c1b998a7b647f70
= <=>
2022-01-01 19:27:24 -0600

    added friend mutation

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,7 +4,8 @@ import { Redirect, useParams } from 'react-router-dom';
 import ThoughtList from '../components/ThoughtList';
 import FriendList from '../components/FriendList';
 
-import { useQuery } from '@apollo/client';
+import { ADD_FRIEND } from '../utils/mutations';
+import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
 import Auth from '../utils/auth';
@@ -12,6 +13,8 @@ import Auth from '../utils/auth';
 
 const Profile = (props) => {
   const { username: userParam } = useParams();
+  //destructure the mutation funfrom to we can use it in a click function
+  const [addFriend] = useMutation(ADD_FRIEND);
 
   //now if there's a value in userParam that we got from the URL bar, wel'll use that to 
   //run the QUERY_USER query if there is no value we visit /profile
@@ -37,12 +40,28 @@ const Profile = (props) => {
     );
   }
 
+  //define the onClick attribute for reference 
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user.id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : 'your'} profile.
         </h2>
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -15,7 +15,7 @@ mutation login($email: String!, $password: String!){
 `;
 
 //create a mutation for creating new users through the form page
-export const ADD_USER = gql `
+export const ADD_USER = gql`
 mutation addUser($username: String!, $email: String!, $password:String!){
     addUser(username: $username, email: $email, password: $password) {
         token
@@ -26,3 +26,16 @@ mutation addUser($username: String!, $email: String!, $password:String!){
     }
 }
 `;
+
+export const ADD_FRIEND = gql`
+mutation addFriend($id: ID!) {
+    addFriend(friendId: $id) {
+    _id
+    username
+    friendCount
+    friends {
+        _id
+        username
+    }
+    }
+}`
