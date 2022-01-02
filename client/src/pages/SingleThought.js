3e7c0886a1c879e0152211dac7aa7ab0403a8fd1
= <=>
2022-01-02 09:22:45 -0600

    added reaction functions

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -38,6 +38,7 @@ const SingleThought = (props) => {
       {thought.reactionCount > 0 && (
         <ReactionList reactions={thought.reactions}
         />)}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
